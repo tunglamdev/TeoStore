9f78ee30b4f5fe85d743c7213965e699cc977c2c
tunglamctu <86045295+tunglamctu@users.noreply.github.com>
2021-09-12 21:51:55 +0700

    Update: Mobile menu link

--- App/Views/client/shared/header.php
+++ App/Views/client/shared/header.php
@@ -47,9 +47,9 @@
                                 <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
                             </label>
                             <ul class="header__mobile-nav-list">
-                                <li class="header__mobile-nav-link"><a class="dropdown-item" href="#"><i class="fas fa-home me-2"></i>Trang chủ</a></li>
-                                <li class="header__mobile-nav-link"><a class="dropdown-item" href="#"><i class="fas fa-seedling me-2"></i>Sản phẩm</a></li>
-                                <li class="header__mobile-nav-link"><a class="dropdown-item" href="#"><i class="fas fa-address-card me-2"></i>Giới thiệu</a></li>
+                                <li class="header__mobile-nav-link"><a class="dropdown-item" href="<?= DOCUMENT_ROOT?>/home"><i class="fas fa-home me-2"></i>Trang chủ</a></li>
+                                <li class="header__mobile-nav-link"><a class="dropdown-item" href="<?= DOCUMENT_ROOT?>/products"><i class="fas fa-seedling me-2"></i>Sản phẩm</a></li>
+                                <li class="header__mobile-nav-link"><a class="dropdown-item" href="<?= DOCUMENT_ROOT?>/about"><i class="fas fa-address-card me-2"></i>Giới thiệu</a></li>
                                 <li class="header__mobile-nav-link"><a class="dropdown-item" href="#"><i class="fas fa-user me-2"></i>Cá nhân</a></li>
                                 <li class="header__mobile-nav-link"><a class="dropdown-item" href="#"><i class="fas fa-cart-plus me-2"></i>Giỏ hàng</a></li>
                                 <li class="header__mobile-nav-link"><a class="dropdown-item" href="#"><i class="fas fa-user-plus me-2"></i>Đăng ký</a></li>
